7eca007179cd5194131ace908b050c599d6fd0c6
fusbudget <andrewsanser@gmail.com>
2022-02-01 09:08:49 +0100

    add functions selectAdd, selectList, selectPay (ex-3)

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ class App extends React.Component {
     super();
 
     this.state = {
-      activeTab: "add",
+      activeTab: "Add",
       items: [],
     };
     this.renderActiveTab = this.renderActiveTab.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.selectAdd = this.selectAdd.bind(this);
+    this.selectList = this.selectList.bind(this);
+    this.selectPay = this.selectPay.bind(this);
   }
   renderActiveTab() {
     switch (this.state.activeTab) {
@@ -33,13 +36,22 @@ class App extends React.Component {
     this.setState({activeTab: e.target.innerText});
     console.log(this.state.activeTab);
   };
+  selectAdd() {
+    this.setState({activeTab: "Add"})
+  }
+  selectList() {
+    this.setState({activeTab: "List"})
+  }
+  selectPay() {
+    this.setState({activeTab: "Pay"})
+  }
   render() {
     return (
       <div>
         <h1>Bakery</h1>
-        <Button onClick={this.handleClick} children={"Add"}></Button>
-        <Button onClick={this.handleClick} children={"List"}></Button>
-        <Button onClick={this.handleClick} children={"Pay"}></Button>
+        <Button onClick={this.selectAdd} children={"Add"}></Button>
+        <Button onClick={this.selectList} children={"List"}></Button>
+        <Button onClick={this.selectPay} children={"Pay"}></Button>
         <h2>{this.state.activeTab}</h2>
 
       </div>
